20564fd0852270e375b65ae981afab3aa6b47146
ES_PREPA_DEV <sosoros66@icloud.com>
2026-01-28 16:23:06 +0100

    add challenge crud

---
+++ www/src/Form/ChallengeType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Challenge;
+use App\Entity\Media;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChallengeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('deadline')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('isActive')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'id',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('medias', EntityType::class, [
+                'class' => Media::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Challenge::class,
+        ]);
+    }
+}
