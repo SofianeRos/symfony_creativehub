d7fe59c2a2e6a1b2579a5a81ad0f1291bba31c5a
ES_PREPA_DEV <sosoros66@icloud.com>
2026-02-04 16:51:21 +0100

    add show user admin

--- www/src/Controller/Admin/UserController.php
+++ www/src/Controller/Admin/UserController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin')]
 final class UserController extends AbstractController
@@ -47,6 +48,14 @@ final class UserController extends AbstractController
             'users' => $users,
             'search' => $search,
             'filter' => $filter
+        ]);
+    }
+
+    #[Route('/user/{id}', name: 'app_admin_user_show')]
+    public function show(User $user): Response
+    {
+        return $this->render('admin/user/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
